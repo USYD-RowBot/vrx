b38e4fe255b9ca2cc9f927b9df972bacb4e307b6
Steven Liu <steeven.liu2@gmail.com>
2019-07-30 13:27:10 +1000

    dont go both right maybe? :(

--- scripts/runforward.py
+++ scripts/runforward.py
@@ -3,7 +3,7 @@ import rospy
 from std_msgs.msg import Float32
 rospy.init_node("runforward")
 pub_left= rospy.Publisher("right_thrust_cmd",Float32,queue_size=10)
-pub_right= rospy.Publisher("right_thrust_cmd",Float32,queue_size=10)
+pub_right= rospy.Publisher("left_thrust_cmd",Float32,queue_size=10)
 rate = rospy.Rate(10)
 while not rospy.is_shutdown():
     pub_left.publish(1)
