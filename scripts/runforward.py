57d0f3f6426d01ecbc50def203a730c48c691a58
Steven Liu <steeven.liu2@gmail.com>
2019-07-30 13:22:44 +1000

    capital p publisher

--- scripts/runforward.py
+++ scripts/runforward.py
@@ -2,8 +2,8 @@
 import rospy
 from std_msgs.msg import Float32
 rospy.init_node("runforward")
-pub_left= rospy.publisher("right_thrust_cmd",Float32,queue_size=10)
-pub_right= rospy.publisher("right_thrust_cmd",Float32,queue_size=10)
+pub_left= rospy.Publisher("right_thrust_cmd",Float32,queue_size=10)
+pub_right= rospy.Publisher("right_thrust_cmd",Float32,queue_size=10)
 while not rospy.is_shutdown():
     pub_left.publish(1)
     pub_right.publish(1)
